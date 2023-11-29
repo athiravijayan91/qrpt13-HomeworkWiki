20dc28118d2032e157d3a8c3ef4e9c046d56f416
Arun Krishna <pkathiravijayan@gmail.com>
2023-11-29 13:47:50 -0700

    added some code

--- 2.7/SpecPage.ts
+++ 2.7/SpecPage.ts
@@ -1,60 +1,34 @@
-import {Builder,By, Capabilities, until, WebDriver,WebElement } from "selenium-webdriver";
-  const chromedriver = require("chromedriver");
+import { By, WebDriver, until} from "selenium-webdriver";
 
-  const driver: WebDriver = new Builder()
-  .withCapabilities(Capabilities.chrome())
-  .build();
 
-  export class SpecPage {
-    //required parameters inorder to use the class outside of the page.
+export class SpecPage {
+    driver: WebDriver;
 
-    driver: WebDriver; //calling driver
-    url: string = "https://www.google.com/"; //set url
+    url: string = "https://www.google.com";
 
-    //adding locators, constructor and methods
+    searchBar: By = By.name('q')
+    results: By = By.id("rso")
 
-    //set locators for this test here we navigate to google, need search bar element, then result element
+    constructor(driver: WebDriver) {
+        this.driver = driver;
+    }
 
-    searchBar: By = By.name('q'); 
-    results: By = By.css('#rcnt'); 
-
-    //this is setting the parameters in order to use them in the methods and outside of the class
-
-    constructor(driver:WebDriver, url: string) {
-        this.driver = driver; 
-        this.url = url; 
-    };
-
-   // Navigate method
-
-   async navigate() {
-    await this.driver.get(this.url); 
-    await this.driver.wait(until.elementLocated(this.searchBar)); 
-}; 
-
-// enteting in the search bar
-
-async sendKeys(elementBy: By, keys) {
-  await this.driver.wait(until.elementLocated(elementBy))
-  return this.driver.findElement(elementBy).sendKeys(keys)
+    async navigate() {
+        await this.driver.get(this.url)
+        await this.driver.wait(until.elementLocated(this.searchBar))
+    }
+    async sendKeys(elementBy: By, keys) {
+        await this.driver.wait(until.elementLocated(elementBy))
+        return this.driver.findElement(elementBy).sendKeys(keys)
+    }
+    async getText(elementBy: By) {
+        await this.driver.wait(until.elementLocated(elementBy))
+        return (await this.driver.findElement(elementBy)).getText()
+    }
+    async doSearch(text: string) {
+        return this.sendKeys(this.searchBar, `${text}\n`)
+    }
+    async getResults() {
+        return this.getText(this.results)
+    }
 }
-
-
-
-async getText(elementBy: By) {
-  await this.driver.wait(until.elementLocated(elementBy))
-  return (await this.driver.findElement(elementBy)).getText()
-}
-
-// searching for the keyword purple
-async doSearch(text: string) {
-  return this.sendKeys(this.searchBar, `${text}\n`)
-}
-async getResults() {
-  return this.getText(this.results)
-}
-
-
-
-
-  }
--- 2.7/specifications.test.ts
+++ 2.7/specifications.test.ts
@@ -11,7 +11,7 @@ const page = new SpecPage(driver);//
 test("it works", async () => {
   await page.navigate();
   await page.doSearch("purple");
-  expect(await page.getResults()).toContain("purple");
+  expect(await page.getResults()).toContain("purple\n");
 });
 
 afterAll(async () => {

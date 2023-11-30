981102d11c74937aab8191acc6b59b8e4d38ce54
Arun Krishna <pkathiravijayan@gmail.com>
2023-11-29 18:35:27 -0700

    added some comments

--- 2.8/basePageTest.test.ts
+++ 2.8/basePageTest.test.ts
@@ -1,27 +1,27 @@
 import {Google} from './googleWBP'
 import { Driver } from 'selenium-webdriver/chrome'
-const fs = require('fs')
+const fs = require('fs')// calling library file system
 const google = new Google()
 
 
 test ('searching ', async()=>{
     await google.navigate()
-    await google.search('Hershey\n')
+    await google.search('Hersheys\n')  // \n is used to hit enter after the search item is typed
     let resultText = await google.getResults()
-    expect (resultText).toContain('Hershey')
+    expect (resultText).toContain('Hersheys')
 
 
 
     await fs.writeFile(`${__dirname}/google.png`,
-    await google.driver.takeScreenshot(), "base64",
+    await google.driver.takeScreenshot(), "base64",   //used .driver here since it is a built in method in selenium
     (e) => {
         if (e) console.error(e)
-        else console.log('Save Succesful')
+        else console.log('Saved Succesfully')
     }
     )
    fs.writeFile(`${__dirname}/test.txt`, resultText, (e) => {
        if (e) console.error(e)
-       else console.log('save Succesful')
+       else console.log('saved Succesfully')
    })
 
 })
